c47f7d748c0affe2e638ea4eb22a0301c28fa191
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-01-27 16:38:36 +1100

    Working grid

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const config = require("./utils/config");
+const moment = require("moment");
 const express = require("express");
 const app = express();
 app.set("view engine", "pug");
 
+
+let items = [
+    {
+        name: "Clock",
+        content: `${moment().format("MMMM Do YYYY, h:mm:ss a")}`,
+        position: [2, 2],
+        size: [2, 2]
+    }
+];
 
 app.use((req, res, next) => {
     res.setHeader("X-Robots-Tag", "none");
@@ -12,7 +22,11 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    let numBoxes = items.reduce((acc, val) => {
+        return acc + val.size[0] * val.size[1];
+    }, 0);
+    console.log(numBoxes);
+    res.render("index", { title: "Homepage", items, numBoxes });
 });
 
 const server = app.listen(config.PORT, () => {
